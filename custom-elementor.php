d46440f2629f282cc5964f61de38e3f0011a6c8a
Alex Y <alx.young234@gmail.com>
2019-11-12 21:14:43 -0700

    Create custom-elementor.php

---
+++ custom-elementor.php
@@ -0,0 +1,37 @@
+<?php
+namespace WPC;
+
+// use Elementor\Plugin; ?????
+
+class Widget_Loader{
+
+  private static $_instance = null;
+
+  public static function instance()
+  {
+    if (is_null(self::$_instance)) {
+      self::$_instance = new self();
+    }
+    return self::$_instance;
+  }
+
+
+  private function include_widgets_files(){
+    require_once(__DIR__ . '/widgets/advertisement.php');
+  }
+
+  public function register_widgets(){
+
+    $this->include_widgets_files();
+
+    \Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Widgets\Advertisement());
+
+  }
+
+  public function __construct(){
+    add_action('elementor/widgets/widgets_registered', [$this, 'register_widgets'], 99);
+  }
+}
+
+// Instantiate Plugin Class
+Widget_Loader::instance();
